b99c0184f0c341f3d9f1cdadd8755b9e528972d0
ShaneMarusczak <marusczaks@gmail.com>
2020-04-26 20:55:05 -0400

    less randomness

--- scripts/battleship_cpu.js
+++ scripts/battleship_cpu.js
@@ -491,7 +491,7 @@ var gameStarted = false;
 
 	const searchingShot = () => {
 		let x, y;
-		if (shotsfired < 5 || randomIntFromInterval(0, 5) === 0) {
+		if (shotsfired < 4 || randomIntFromInterval(0, 8) === 0) {
 			do {
 				x = randomIntFromInterval(0, 8);
 				y = randomIntFromInterval(0, 8);
