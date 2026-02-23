3d0602f9b7cb1b513c8d60a82867c9a1aeea99ef
ShaneMarusczak <marusczaks@gmail.com>
2026-02-23 12:15:14 -0500

    style updates, no dark mode

--- js/shared.js
+++ js/shared.js
@@ -24,62 +24,7 @@ var compMoveWindow;
 var gameOver = false;
 var gameStarted = false;
 
-function setDarkMode() {
-  Array.from(document.getElementsByTagName("p")).forEach((p) =>
-    p.classList.add("lightText")
-  );
-  document.getElementsByTagName("body")[0].classList.add("darkBackground");
-  document.getElementsByTagName("html")[0].classList.add("darkBackgroundHTML");
-  document.getElementById("thinking").classList.add("darkGrayBackground");
-  document.getElementById("githubicon").classList.add("lightBackground");
-  document.getElementById("homeIcon").classList.add("lightBackground");
-  document.getElementById("downArrow").classList.add("lightBackground");
-  document.getElementById("rotateArrow").classList.add("lightBackground");
-  document.getElementById("leftList").classList.add("darkGrayBackground");
-  document.getElementById("rightList").classList.add("darkGrayBackground");
-  document.getElementById("ships").classList.add("darkGrayBackground");
-  document.getElementById("gameboard").classList.add("darkGrayBackground");
-  document.getElementById("gameboard_cpu").classList.add("darkGrayBackground");
-  document
-    .getElementsByClassName("animation")[0]
-    .classList.add("darkGrayBackground");
-  window.setCookie("darkMode", "Y", 1);
-}
-
-function removeDarkMode() {
-  Array.from(document.getElementsByTagName("p")).forEach((p) =>
-    p.classList.remove("lightText")
-  );
-  document.getElementsByTagName("body")[0].classList.remove("darkBackground");
-  document
-    .getElementsByTagName("html")[0]
-    .classList.remove("darkBackgroundHTML");
-  document.getElementById("thinking").classList.remove("darkGrayBackground");
-  document.getElementById("githubicon").classList.remove("lightBackground");
-  document.getElementById("homeIcon").classList.remove("lightBackground");
-  document.getElementById("downArrow").classList.remove("lightBackground");
-  document.getElementById("rotateArrow").classList.remove("lightBackground");
-  document.getElementById("leftList").classList.remove("darkGrayBackground");
-  document.getElementById("rightList").classList.remove("darkGrayBackground");
-  document.getElementById("ships").classList.remove("darkGrayBackground");
-  document.getElementById("gameboard").classList.remove("darkGrayBackground");
-  document
-    .getElementById("gameboard_cpu")
-    .classList.remove("darkGrayBackground");
-  document
-    .getElementsByClassName("animation")[0]
-    .classList.remove("darkGrayBackground");
-  window.setCookie("darkMode", "N", 1);
-}
-
 (() => {
-  document.getElementById("darkMode").addEventListener("click", () => {
-    if (window.getCookie("darkMode") === "N") {
-      window.setDarkMode();
-    } else {
-      window.removeDarkMode();
-    }
-  });
   document
     .getElementById("resetWinLoss")
     .addEventListener("click", window.resetWinLoss);
